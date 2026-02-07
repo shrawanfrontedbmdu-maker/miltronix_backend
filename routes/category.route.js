eba6f8d16de310d182273ea38778d575d4699cea
Shrawan kr kamat <shrawam.fontend.bmdu@gmail.com>
2026-02-07 10:20:15 +0530

    updated

--- routes/category.route.js
+++ routes/category.route.js
@@ -1,18 +1,29 @@
 import express from "express";
 import {
   createCategory,
+  updateCategory,
   deleteCategory,
-  editCategory,
   getCategories,
-  getParentCategories,
+  getCategoryById,
 } from "../controllers/category.controller.js";
 import upload from "../middlewares/multer.js";
+
 const router = express.Router();
 
-router.post("/", upload.single("icon"), createCategory);
-router.put("/:id", upload.single("icon"), editCategory);
+const uploadFields = upload.fields([
+  { name: "image", maxCount: 1 },
+  { name: "infoSectionImage", maxCount: 1 },
+  { name: "cards[0][image]" },
+  { name: "cards[1][image]" },
+  { name: "cards[2][image]" },
+  { name: "cards[3][image]" },
+  { name: "cards[4][image]" },
+]);
+
+router.post("/", uploadFields, createCategory);
+router.put("/:id", uploadFields, updateCategory);
 router.get("/", getCategories);
-router.get("/parent", getParentCategories);
+router.get("/:id", getCategoryById);
 router.delete("/:id", deleteCategory);
 
 export default router;
