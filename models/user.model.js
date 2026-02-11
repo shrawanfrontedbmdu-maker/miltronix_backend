e001596dfee8be7a3a1c5e92d790d712a4b31e82
Shrawan kr kamat <shrawam.fontend.bmdu@gmail.com>
2026-02-11 10:25:25 +0530

    updated

--- models/user.model.js
+++ models/user.model.js
@@ -2,13 +2,30 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema(
   {
-    fullName: { type: String, required: true },
+    fullName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
 
-    email: { type: String, unique: true, sparse: true },
+    email: {
+      type: String,
+      unique: true,
+      sparse: true,
+      lowercase: true,
+      trim: true,
+    },
 
-    mobile: { type: String, unique: true, sparse: true }, 
+    mobile: {
+      type: String,
+      unique: true,
+      sparse: true,
+      trim: true,
+    },
 
-    password: { type: String }, 
+    password: {
+      type: String,
+    },
 
     otp: String,
     otpExpiry: Date,
@@ -16,9 +33,16 @@ const userSchema = new mongoose.Schema(
     resetOtp: String,
     resetOtpExpiry: Date,
 
-    isVerified: { type: Boolean, default: false },
+    isVerified: {
+      type: Boolean,
+      default: false,
+    },
 
-    googleId: { type: String, unique: true, sparse: true }, 
+    googleId: {
+      type: String,
+      unique: true,
+      sparse: true,
+    },
   },
   { timestamps: true }
 );
