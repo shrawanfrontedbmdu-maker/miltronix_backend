d5ac32d6bd84699daa72094955980fd30b2f91c2
adil-bmdu <adil-bmdu@gmail.com>
2026-02-16 12:33:58 +0530

    updates

--- models/user.model.js
+++ models/user.model.js
@@ -34,7 +34,7 @@ const userSchema = new mongoose.Schema(
       select: false,
     },
 
-    role: {
+    role:{
       type: String,
       enum: ["user", "storeManager", "admin"],
       default: "user",
