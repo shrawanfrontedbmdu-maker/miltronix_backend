438f72cf851e9147a121d2f0312f04c1bc9fdf00
Shrawan kr kamat <shrawam.fontend.bmdu@gmail.com>
2026-02-24 13:33:50 +0530

    updated

--- routes/product.route.js
+++ routes/product.route.js
@@ -7,6 +7,7 @@ import {
   deleteProduct,
   getFeaturedProducts,
   getRecommendedProducts,
+  getTopDealProducts, 
   searchProducts,
   searchSuggestions,
 } from "../controllers/product.controller.js";
@@ -15,11 +16,7 @@ import upload from "../middlewares/multer.js";
 const router = express.Router();
 
 /* ================= CREATE PRODUCT ================= */
-router.post(
-  "/",
-  upload.any(),
-  createProduct
-);
+router.post("/", upload.any(), createProduct);
 
 /* ================= GET ALL PRODUCTS ================= */
 router.get("/", getProducts);
@@ -32,18 +29,16 @@ router.get("/featured", getFeaturedProducts);
 
 router.get("/recommended", getRecommendedProducts);
 
+/* ================= GET TOP DEAL PRODUCTS ================= */ 
+router.get("/top-deals", getTopDealProducts);
+
 /* ================= GET PRODUCT BY ID ================= */
-router.get("/:id", getProductById);
+router.get("/:id", getProductById); 
 
 /* ================= UPDATE PRODUCT ================= */
-router.put(
-  "/:id",
-  upload.any(),
-  updateProduct
-);
+router.put("/:id", upload.any(), updateProduct);
 
 /* ================= DELETE PRODUCT ================= */
 router.delete("/:id", deleteProduct);
-
 
 export default router;
